9caba83ce1446b9a99e1a31ebf581c334663378d
Oscar Guzman <oguzman@procibernetica.com>
2018-10-12 09:17:57 -0500

    calcu complete

--- docs/assets/js/calcu.js
+++ docs/assets/js/calcu.js
@@ -1,43 +1,94 @@
-let temporalValue=0;
+let temporalValueDisplay    = 0
+let nextValueDisplay        = null
+let previusValueDisplay     = null
+let temporalOperator        = null
+let previusOperator         = null
+let resultOperation         = 0
 
-let calculadora_button = document.getElementsByClassName("calculadora-button");
-let calculadora_diplay_number = document.getElementById("calculadora-diplay-number") ;
-
-let setDisplatNumber =  (value) => {
-
-    temporalValue = ( temporalValue == 0 )?value: temporalValue+=value;
-
-    calculadora_diplay_number.setAttribute('value', temporalValue) 
-    calculadora_diplay_number.innerHTML = temporalValue
-}
-
-let getValueButton = (btn) =>  {
-    if (btn.value == "clean" ) {temporalValue = 0; return 0; }
-    if (!isNaN(btn.value)) { operate(btn.value, temporalValue ) } 
-    else { return btn.value }
-    
+const add = () =>  Number( previusValueDisplay) + Number(nextValueDisplay)
+const rest = () => Number( previusValueDisplay) - Number(nextValueDisplay)
+const multiply = () =>   (Number( previusValueDisplay) * Number(nextValueDisplay))
+const divide = () => {
+    if ( Number(nextValueDisplay) == 0 ) { nextValueDisplay = 1  }
+    return  ( Number( previusValueDisplay) / Number(nextValueDisplay) ) 
 } 
 
-for (let btn of calculadora_button ) {
-    btn.addEventListener("click", () => { setDisplatNumber( getValueButton(btn) ) } )
+//get interface values
+let calculadora_button = document.getElementsByClassName("calculadora-button")
+let calculadora_diplay_number = document.getElementById("calculadora-diplay-number")
+
+//set triggers btns 
+for (let btn of calculadora_button ) { btn.addEventListener("click", () => { mainActionBtn(btn.value) })}
+
+//set value on inteface display 
+//temporalValueDisplay = ( temporalValueDisplay == 0 )?newValueBtn: (temporalValueDisplay+=newValueBtn).toString()
+const setValueInterfaceDisplay = valueToDisplay => { calculadora_diplay_number.innerHTML = valueToDisplay }
+
+//numero o simbolo
+
+const mainActionBtn = (newValueBtn) => {
+    
+    if (!isNaN(newValueBtn) ){ 
+        newValueBtn = newValueBtn.toString()
+        temporalValueDisplay = ( temporalValueDisplay == 0 )?newValueBtn: temporalValueDisplay+=newValueBtn
+        setValueInterfaceDisplay(temporalValueDisplay)
+    } else {
+        previusOperator =  (newValueBtn!="=") ?previusOperator = newValueBtn : previusOperator
+
+        if (newValueBtn == "clean") { clean(); return; }
+
+        if (!previusValueDisplay){
+            
+            previusValueDisplay = temporalValueDisplay
+            temporalValueDisplay = 0
+            setValueInterfaceDisplay(previusValueDisplay)
+            temporalOperator = newValueBtn
+        } else {
+            nextValueDisplay = temporalValueDisplay
+            temporalValueDisplay = 0
+            resultOperation =  execOperation(newValueBtn)
+            setValueInterfaceDisplay( "= " + resultOperation)
+            nextValueDisplay = 0 
+            temporalOperator = null
+            previusValueDisplay = resultOperation
+            
+        }
+
+    }
 }
 
+const execOperation = (temporalOperator) => {
+    switch (temporalOperator) {
 
-const operate = ( simbol, value ) => {
-    switch(simbol){
-        case "+" :
-            setDisplatNumber(temporalValue+=value);
-            break;
-        case "-" :
-            setDisplatNumber( temporalValue-=value);
-            break;
-        case "*" :
-            setDisplatNumber(temporalValue*=value);
-            break;
-        case "/" :
-            setDisplatNumber( temporalValue/=value);
-            break;
+        case "+":
+            return add()
+            
+        case "-":
+            return rest()
+    
+        case "*":
+            return multiply()
+
+        case "/":
+            return divide()
+
+        case "=":
+            return execOperation(previusOperator);
+
+        case "clean":
+            clean()
+            return 0
+
         default:
-            alert("seleccione un operador")
+            return "ups.." 
+
     }
+}
+
+const clean = () => {
+    nextValueDisplay = null
+    previusValueDisplay = null
+    temporalOperator = null
+    temporalValueDisplay = 0
+    setValueInterfaceDisplay(temporalValueDisplay)
 }
